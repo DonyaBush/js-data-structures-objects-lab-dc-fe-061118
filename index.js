d594375922ea8cc99eee7b8d4137f3667a325913
Ki Ki  Bush <donyabush@gmail.com>
2018-07-29 18:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution in this file!
 driver = {}
-function updateDriverWithKeyAndValue(driver, key, value) {
-copy=Object.assign({}, driver)
+
  copy[key]=value;
   return copy
   }
